82627886a627f690298ba450961c3449bab45261
lucas29-29 <65615306+lucas29-29@users.noreply.github.com>
2023-12-13 11:47:24 +0100

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,45 +1,41 @@
-export default {
-  // ...
-  router: {
-    base: '/Portfolio-Lucas--Constant/'
-  },
-  // ...
-}
+import { defineNuxtConfig } from 'nuxt3';
+
 export default defineNuxtConfig({
-	ssr: false,
-	app: {
-		pageTransition: {name: 'page', mode: 'out-in', appear: true},
-	},
-	vue: {
-		compilerOptions: {
-			isCustomElement: tag => tag === 'lottie-player'
-		}
-	},
-	runtimeConfig: {
-		spotifyClientId: process.env.SPOTIFY_CLIENT_ID,
-		spotifyClientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-		spotifyRefreshToken: process.env.SPOTIFY_REFRESH_TOKEN
-	},
-	vite: {
-		css: {
-			preprocessorOptions: {
-				scss: {
-					additionalData: '@use "@/assets/scss/style.scss" as *;'
-				}
-			}
-		}
-	},
-	modules: [
-		'@nuxt/content', '@nuxt/image-edge'
-	],
-	content: {
-		navigation: {
-			fields: ['title', 'slug', 'type']
-		},
-		locales: ['fr', 'en'],
-		defaultLocale: 'fr'
-	},
-	image: {
-		provider: 'netlify',
-	}
-})
+  ssr: false,
+  app: {
+    pageTransition: { name: 'page', mode: 'out-in', appear: true },
+  },
+  vue: {
+    compilerOptions: {
+      isCustomElement: (tag) => tag === 'lottie-player',
+    },
+  },
+  runtimeConfig: {
+    spotifyClientId: process.env.SPOTIFY_CLIENT_ID,
+    spotifyClientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+    spotifyRefreshToken: process.env.SPOTIFY_REFRESH_TOKEN,
+  },
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "@/assets/scss/style.scss" as *;',
+        },
+      },
+    },
+  },
+  modules: ['@nuxt/content', '@nuxt/image-edge'],
+  content: {
+    navigation: {
+      fields: ['title', 'slug', 'type'],
+    },
+    locales: ['fr', 'en'],
+    defaultLocale: 'fr',
+  },
+  image: {
+    provider: 'netlify',
+  },
+  router: {
+    base: '/Portfolio-Lucas--Constant/',
+  },
+});
